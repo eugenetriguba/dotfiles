935e931e105b82aaafe9efad319cc8c0067738f6
Eugene Triguba <eugenetriguba@gmail.com>
2020-09-21 21:33:36 -0500

    Move nvm above yarn export

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -13,9 +13,9 @@ export PATH="$HOME/.poetry/bin:$PATH"
 # Pyenv
 export PATH="$HOME/.pyenv/bin:$PATH"
 
-# Yarn
-export PATH="$PATH:$(yarn global bin)"
-
 # NVM
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Yarn
+export PATH="$PATH:$(yarn global bin)"
