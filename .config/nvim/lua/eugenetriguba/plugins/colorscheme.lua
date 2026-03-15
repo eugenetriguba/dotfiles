ff2cf7e64b3a92397d2a09c63869bac46002fa2d
Eugene Triguba <hello@eugenetriguba.com>
2026-03-15 12:26:34 -0400

    Add nord colorscheme for nvim

--- .config/nvim/lua/eugenetriguba/plugins/colorscheme.lua
+++ .config/nvim/lua/eugenetriguba/plugins/colorscheme.lua
@@ -1,12 +1,16 @@
 vim.pack.add {
   {
-    name = 'tokyonight',
-    src = 'https://github.com/folke/tokyonight.nvim',
+    name = 'nord',
+    src = 'https://github.com/shaunsingh/nord.nvim',
   },
 }
-require('tokyonight').setup {
-  styles = {
-    comments = { italic = false },
-  },
-}
-vim.cmd.colorscheme 'tokyonight'
+
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = true
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_italic = false
+vim.g.nord_bold = false
+
+require('nord').set()
+vim.cmd.colorscheme 'nord'
