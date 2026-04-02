77859148a3a11be4705c4290a4ba146d7b3ccf07
Eugene Triguba <hello@eugenetriguba.com>
2026-04-01 22:01:36 -0400

    Add et-is-desktop-p for conditional emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,10 +3,14 @@
 ;; Add configuration modules to load path
 (add-to-list 'load-path '"~/.dotfiles/.emacs.d/modules")
 
+;; --------- Core Utilities ---------------------
+(defun et-is-desktop-p ()
+  (string-match-p "desktop" (system-name)))
+
 ;; --------- Core Variables ---------------------
 (defvar et-monospace-font "Hack Nerd Font Mono"
   "The font used by default and for any monospaced font usage.")
-(defvar et-monospace-font-size 160)
+(defvar et-monospace-font-size (if (et-is-desktop-p) 192 160))
 
 ;; --------- Modules ----------------------------
 (require 'et-package)
