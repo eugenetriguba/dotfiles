b7c5166aa3281517b43fac2e409cecfa712cfd58
Eugene Triguba <hello@eugenetriguba.com>
2026-04-18 21:10:10 -0400

    Update emacs custom vars

--- .emacs.d/custom-vars.el
+++ .emacs.d/custom-vars.el
@@ -4,7 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(apheleia corfu doom-modeline embark-consult evil-collection flycheck
+	      magit marginalia nord-theme orderless sly surround
+	      treesit-auto vertico))
  '(safe-local-variable-values
    '((eval remove-hook 'flymake-diagnostic-functions
 	   #'elisp-flymake-checkdoc t)
