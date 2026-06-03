65ecfc5368b4e2d8f6e4ea0470d87260bb5579b9
Eugene Triguba <hello@eugenetriguba.com>
2026-06-03 12:58:28 +0000

    Add cs6340 changes

--- .emacs.d/early-init.el
+++
@@ -1,7 +0,0 @@
-;;; early-init.el --- Early init -*- lexical-binding: t; -*-
-;;; Commentary:
-;;; Code:
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(require 'et-early)
-(provide 'early-init)
-;;; early-init.el ends here
